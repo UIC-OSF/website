d4dca3a4713adb93c63fe0d7a89116511bc2878e
Blake Bertuccelli-Booth <46652+bbertucc@users.noreply.github.com>
2025-12-11 11:24:02 -0600

    small update

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -65,7 +65,7 @@ export const Team: React.FC = () => {
 
                     <TeamMember
                         name="Stefin Pasternak"
-                        bio="Leading AI Leaders, while managing the fund's strategic initiatives to gaurantee equitable and sustainableoutcomes."
+                        bio="Leading AI Leaders, while managing initiatives that gaurantee equitable and sustainable outcomes."
                         imageUrl={stefinImg}
                         linkedinUrl="https://www.linkedin.com/in/stephen-pasternak-11979b155/"
                         websiteUrl="https://www.stefinpasternak.com/"
